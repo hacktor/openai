2f2c4de7ae98f5c06c612f05e09637d0fdbe3427
Ruben de Groot <ruben@hacktor.com>
2022-12-11 23:30:33 +0100

    initial

---
+++ app.py
@@ -0,0 +1,39 @@
+import os
+import argparse
+import openai
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, StringField
+
+app = Flask(__name__, template_folder='templates')
+app.config['SECRET_KEY'] = os.urandom(32)
+
+def getsong(artist):
+    """ get possible songtext based on parameter artist """
+    prompt_text = f"Artist: {artist}\n\nLyrics:\n"
+
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt_text,
+        temperature=0.7,
+        max_tokens=256,
+        frequency_penalty=0.5
+    )
+    return prompt_text + response['choices'][0]['text']
+
+# forms
+class ArtistForm(FlaskForm):
+    artist = StringField(label='Artist', render_kw={"placeholder": "Artist"})
+    submit = SubmitField("submit")
+
+@app.route('/', methods=['GET', 'POST'])
+def index(song=None):
+    form = ArtistForm()
+    if form.submit.data:
+        print("validated")
+        song = getsong(form.artist.data)
+    print("UNvalidated")
+    return render_template('home.html', form=form, song=song)
+
+if __name__ == '__main__':
+   app.run()
